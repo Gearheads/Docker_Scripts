a7451942a26cd3ce45078d4927fbc572915200ca
Robert Casale <rc672@scarletmail.rutgers.edu>
2016-09-15 10:55:36 -0400

    Uploaded changes

--- promotion_DTR2.py
+++ promotion_DTR2.py
@@ -1,6 +1,6 @@
 # Rob Casale
-# 4/22/2016
-# This will pull and image from the CDL and promote the image to DC1 and DC2
+# 8/3/2016
+# This will pull an image from the DTR1 and migrate the image to DTR2
 # The image that gets pulled is specified by the <repository>/<image-name>:<tag>
 # <repository> is required
 # <image-name> is optional
@@ -24,21 +24,20 @@ cli = Client(base_url='unix://var/run/docker.sock')
 gcontext = ssl.SSLContext(ssl.PROTOCOL_TLSv1)
 
 # DTR urls
-test_dtr_url='dtr.predit-cdl.es.ad.adp.com'
+cdl_dtr2_url='dtr.cdl.es.ad.adp.com'
 cdl_dtr_url='dtr.dit-cdl.es.ad.adp.com'
-dc1_dtr_url='dtr.prod-dc1.es.ad.adp.com'
-dc2_dtr_url='dtr.prod-dc2.es.ad.adp.com'
 
 # API url requests
-test_dtr_repo_url='https://dtr.predit-cdl.es.ad.adp.com/api/v0/repositories/'
+cdl_dtr2_repo_url='https://dtr.cdl.es.ad.adp.com/api/v0/repositories/'
 cdl_dtr_repo_url='https://dtr.dit-cdl.es.ad.adp.com/api/v0/repositories/'
-dc1_dtr_repo_url='https://dtr.prod-dc1.es.ad.adp.com/api/v0/repositories/'
-dc2_dtr_repo_url='https://dtr.prod-dc2.es.ad.adp.com/api/v0/repositories/'
 
-# Login credentials
+# Login credentials for DTR1
 username='casaler'
 password='adpadp11'
 email='robert.casale@adp.com'
+
+# Login credentials for DTR2
+username2='admin'
 
 # DTR login responses
 login_status='Status'
@@ -60,6 +59,13 @@ proxies={
 def build_request(requestUrl):
 	request=urllib2.Request(requestUrl)
 	base64string = base64.encodestring('%s:%s' % (username, password)).replace('\n', '')
+	request.add_header("Authorization", "Basic %s" % base64string)
+	return request
+
+# Build request
+def build_request_DTR2(requestUrl):
+	request=urllib2.Request(requestUrl)
+	base64string = base64.encodestring('%s:%s' % (username2, password)).replace('\n', '')
 	request.add_header("Authorization", "Basic %s" % base64string)
 	return request
 
@@ -100,10 +106,8 @@ elif len(sys.argv) != 3:
 # Looks for the given tag in a specific image
 def find_tag(tag_list_url, image_name):
 	# Used to push/pull/tag the image(s) properly
-	test_image_name=test_dtr_url + '/' + repo + '/' + image_name
+	dtr2_image_name=cdl_dtr2_url + '/' + repo + '/' + image_name
 	cdl_image_name=cdl_dtr_url + '/' + repo + '/' + image_name
-	dc1_image_name=dc1_dtr_url + '/' + repo + '/' + image_name
-	dc2_image_name=dc2_dtr_url + '/' + repo + '/' + image_name
 	try:
 		tag_response=get_info(build_request(tag_list_url))
 		tag_list=tag_response[tags]
@@ -117,82 +121,79 @@ def find_tag(tag_list_url, image_name):
 		if pulled:
 			print repo + '/' + image_name + ':' + tag + ' pulled'
 			try:
-				#get_code(build_request(test_dtr_repo_url + repo + '/' + image_name))
-                                get_code(build_request(dc1_dtr_repo_url + repo + '/' + image_name))
-                                get_code(build_request(dc2_dtr_repo_url + repo + '/' + image_name))
-                                try:
-				    # Login to DC1 and DC2
-				    #cli.login(username, password, email, test_dtr_url)
-				    cli.login(username, password, email, dc1_dtr_url)
-				    try:
-				    	cli.login(username, password, email, dc2_dtr_url)
-				    	# Push to DC1 and DC2
-				    	#cli.tag(cdl_image_name + ':' + tag, test_image_name, tag)
-                                        cli.tag(cdl_image_name + ':' + tag, dc1_image_name, tag)
-                                        cli.tag(cdl_image_name + ':' + tag, dc2_image_name, tag)
-				    	#cli.push(test_image_name, tag, stream=False)
-				    	cli.push(dc1_image_name, tag, stream=False)
-				    	cli.push(dc2_image_name, tag, stream=False)
-                                        # Once complete remove the images
-				        cli.remove_image(cdl_image_name + ':' + tag)
-				        #cli.remove_image(test_image_name + ':' + tag)
-				        cli.remove_image(dc1_image_name + ':' + tag)
-				        cli.remove_image(dc2_image_name + ':' + tag)
-				        print repo + '/' + image_name + ':' + tag + ' removed'
-				        print 'exit(0): success'
-				    except Exception as e:
-				    	print 'exit(8): Failed to login to DC2 DTR'
-				    	print e
+				get_code(build_request_DTR2(cdl_dtr2_repo_url + repo + '/' + image_name))
+				try:
+				    cli.login(username2, password, email, cdl_dtr2_url)
+				    
+				    # Tag image with the new DTR and then push the image
+				    cli.tag(cdl_image_name + ':' + tag, dtr2_image_name, tag)
+				    cli.push(dtr2_image_name, tag, stream=False)
+				    # Once complete remove the images
+				    cli.remove_image(cdl_image_name + ':' + tag)
+				    cli.remove_image(dtr2_image_name + ':' + tag)
+				        
+				    print repo + '/' + image_name + ':' + tag + ' removed'
+				    print 'exit(0): success'
+				    
 				except Exception as e:
-				    print 'exit(8): Failed to login to DC1 DTR'
+				    print 'exit(8): Failed to login to DTR2'
 				    print e
 			# Repository is not created, therefore repository needs to be created
 			except Exception as e:
 				# Get repository details
 				repo_details=get_info(build_request(cdl_request_url))
 				repo_visibility=repo_details[visibility]
-                                try:
-				    # Create repo on DC1 and DC2
-				    #create_repository_code(build_request(test_dtr_repo_url + repo), image_name, repo_visibility)
-				    create_repository_code(build_request(dc1_dtr_repo_url + repo), image_name, repo_visibility)
-				    create_repository_code(build_request(dc2_dtr_repo_url + repo), image_name, repo_visibility)
+				try:
+				    # Create repo
+				    create_repository_code(build_request_DTR2(cdl_dtr2_repo_url + repo), image_name, repo_visibility)
 				    try:
-				    	# Login to DC1 and DC2
-				    	#cli.login(username, password, email, test_dtr_url)
-				    	cli.login(username, password, email, dc1_dtr_url)
-				    	try:
-				    		cli.login(username, password, email, dc2_dtr_url)
-				    		# Push to DC1 and DC2
-				    		#cli.tag(cdl_image_name + ':' + tag, test_image_name, tag)
-                                                cli.tag(cdl_image_name + ':' + tag, dc1_image_name, tag)
-                                                cli.tag(cdl_image_name + ':' + tag, dc2_image_name, tag)
-				    		#cli.push(test_image_name, tag, stream=False)
-				    		cli.push(dc1_image_name, tag, stream=False)
-				    		cli.push(dc2_image_name, tag, stream=False)
-                                                # Once complete remove the images
-				                cli.remove_image(cdl_image_name + ':' + tag)
-				                #cli.remove_image(test_image_name + ':' + tag)
-				                cli.remove_image(dc1_image_name + ':' + tag)
-				                cli.remove_image(dc2_image_name + ':' + tag)
-				                print repo + '/' + image_name + ':' + tag + ' removed'
-				                print 'exit(0): success'
-				    	except Exception as e:
-				    		print 'exit(8): Failed to login to DC2 DTR'
-				    		print e
+				    	# Login to DTR2
+				    	cli.login(username2, password, email, cdl_dtr2_url)
+
+			    		# Push to DTR2
+			    		cli.tag(cdl_image_name + ':' + tag, dtr2_image_name, tag)
+			    		cli.push(dtr2_image_name, tag, stream=False)
+					# Once complete remove the images
+					cli.remove_image(cdl_image_name + ':' + tag)
+					cli.remove_image(dtr2_image_name + ':' + tag)
+					print repo + '/' + image_name + ':' + tag + ' removed'
+					print 'exit(0): success'
+					
 				    except Exception as e:
-				    	print 'exit(8): Failed to login to DC1 DTR'
-				    	print e
-                                except Exception as e:
-                                    print 'exit(4): Cannot create repository' + \
-                                          ', account \"' + repo + '\" does not exist'
-                                    print 'Exception: ' + str(e)
+					print 'exit(8): Failed to login to DTR2'
+					print e
+				except Exception as e:
+				    print 'exit(4): Cannot create repository' + \
+				          ', account \"' + repo + '\" does not exist'
+				    print 'Exception: ' + str(e)
 		else:
 			print 'exit(3): "' + repo + '/' + image_name + \
-				'" with tag "' + tag + '" was not found'
+			      '" with tag "' + tag + '" was not found'
 	except Exception as e:
 		# repository or image does not exist
 		print 'exit(8): Repository or image does not exist'
 		print 'Exception: ' + str(e)
+
+# Pull all tags for an image
+def pull_images(tag_list_url, image_name):
+	cdl_image_name=cdl_dtr_url + '/' + repo + '/' + image_name
+	try:
+		tag_response=get_info(build_request(tag_list_url))
+		tag_list=tag_response[tags]
+		for tag_version in tag_list:
+			print 'Pulling image...'
+			cli.pull(cdl_image_name, tag_version[name], stream=False)
+			print repo + '/' + image_name + ':' + tag_version[name] + ' pulled'
+	except Exception as e:
+		# repository or image does not exist
+		print 'exit(8): Repository or image does not exist'
+		print 'Exception: ' + str(e)
+
+# Create a repository for each image
+def create_repos(tag_list_url, image_name):
+
+# Push all tags for an image
+def push_images(tag_list_url, image_name):
 
 # Grab  values from user
 repository_image=sys.argv[1].split('/')
